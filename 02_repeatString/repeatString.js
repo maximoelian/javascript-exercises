80a43d4c934de44662e23a14bb226d2c9a0cc2f3
elian <jaimezmaximo@gmail.com>
2024-09-03 21:32:23 -0300

    exercise 1 and 2 completed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let aux  = "";
+    if(num >= 0){
+        for(let i = 1 ; i <=num;i++){
+        
+            aux += string
+        
+        }
+        string = aux;
+    }else{ string = "ERROR";}
+    return string
 };
 
 // Do not edit below this line
