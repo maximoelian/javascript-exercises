193d0678b22974f0db45986cc28d358bfdcb7ac6
elian <jaimezmaximo@gmail.com>
2024-09-04 16:23:06 -0300

    stuck in exercise 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let stringNum = string.length;
+let newString = "";
+for(let i = stringNum-1; i>=0;i--){
+    newString += string[i];
+}
+return newString
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array,num) {
+     let arrayNun = new;
 
+
+    for(let i = 0; i <= array.length - 1; i++){
+        if(array[i] == num){
+            array[i] = array[i+1]
+            num = array[i+1]
+        }
+
+    }
+    return array;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
