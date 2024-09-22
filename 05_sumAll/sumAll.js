2957bb99f7f93c579a6127eac9c09852591c918a
elian <jaimezmaximo@gmail.com>
2024-09-22 18:28:12 -0300

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -7,17 +7,14 @@ let div2=0
 div1 = num1 % 1;
 div2 = num2 % 1;
 
-let checkNumber1= isNaN(num1);
-let checkNumber2=isNaN(num2);
 
 if(num1 > num2){
     aux = num2;
     num2 = num1;
     num1 = aux;
  }
- if (num1 < 0 || num2 < 0 || div1 != 0 || div2 != 0 || checkNumber1 != "false" || checkNumber2 != "false") {
-    cont="ERROR";    
- } else {
+ if (num1 < 0 || num2 < 0 || div1 != 0 || div2 != 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) return cont="ERROR"    
+ else {
 
     for(let i=num1; i <=num2; i++){
         cont+=i;
