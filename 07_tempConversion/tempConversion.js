46b73dbcb1194e75d15087930cc09222ae8f4ef3
elian <jaimezmaximo@gmail.com>
2024-09-26 12:02:39 -0300

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fGrade) {
+  newTemp = (fGrade -32) * 0.5556;
+  roundedTemp = newTemp.toFixed(1)
+  return parseFloat(roundedTemp);  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cGrade) {
+  newTemp = (cGrade * 1.8) +32;
+  roundedTemp = newTemp.toFixed(1)
+  return parseFloat(roundedTemp);  
 };
 
 // Do not edit below this line
